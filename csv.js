c61e44fd5cc2c75082fe5b4ae593f4f5e8288a4a
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-04-20 02:37:55 -0700

    sped up search of data

--- csv.js
+++ csv.js
@@ -1,25 +1,61 @@
 const fs = require('fs');
+var SortedMap = require("collections/sorted-map");
 
 class Data {
   constructor(columns, rows){
     this.columns = columns;
     this.rows = rows;
+    this.maps = [];
+    for(let i = 0; i < this.columns.length; i++){
+      this.maps.push(new SortedMap());
+    }
+    for(let i = 0; i < this.rows.length; i++){
+      for(let j = 0; j < this.rows[i].length; j++){
+        if(this.maps[j].has(rows[i][j])){
+          this.maps[j].get(this.rows[i][j]).push(i);
+        }
+        else{
+          this.maps[j].set(this.rows[i][j], [i]);
+        }
+      }
+    }
+    for(let i = 0; i < this.maps.length; i++){
+      for(let j = 0; j < this.maps[i].length; j++){
+        if(this.maps[i].has(j)){
+          this.maps[i].get(j).sort();
+        }
+      }
+    }
   }
   
   searchIndex(columnsIndex, values){
     if(columnsIndex.length != values.length){
       return null;
     }
+    let possibleResults = [];
     let results = [];
-    for(let i = 0; i < this.rows.length; i++){
-      let j = 0
-      for(; j < columnsIndex.length; j++){
-        if(this.rows[i][columnsIndex[j]] != values[j]){
-          break;
-        }
+    if(columnsIndex.length == 1){
+      possibleResults = this.maps[columnsIndex[0]].get(values[0]);
+      for(let i = 0; i < possibleResults.length; i++){
+        results.push(this.rows[possibleResults[i]]);
       }
-      if(j == columnsIndex.length){
-        results.push(this.rows[i]);
+      return results;
+    }
+    for(let i = 0; i < columnsIndex.length; i++){
+      let newResults = this.maps[i].get(values[i]);
+      if(newResults != undefined){
+        possibleResults.concat(newResults);
+      }
+    }
+    possibleResults.sort();
+    for(let i = 0; i < possibleResults.length; i++){
+      numberOfResults = 0;
+      let j = 1;
+      while(i+j < possibleResults.length && possibleResults[i+j] == possibleResults[i]){
+        j++;
+      }
+      if(j >= columnsIndex.length){
+        results.push(this.rows[possibleResults[i]]);
       }
     }
     return results;
