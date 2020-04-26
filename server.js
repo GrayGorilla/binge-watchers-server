444f44616129ead817cfdc99d51abf479f751843
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2020-04-25 23:31:45 -0700

    made a basic put command

--- server.js
+++ server.js
@@ -5,10 +5,17 @@ const bodyParser = require('body-parser');
 
 const parseCSV = csv.parseCSV;
 
+const start = Date.now();
 let data = parseCSV("data/USvideos.csv");
+console.log("Search time: ", Date.now()- start);
+
 
 var app = express();
 const port = 5000;
+
+//const start = Date.now()
+//data.searchText(["channel_title"],["Vox"]);
+//console.log("Search time: ", Date.now()- start);
 
 // Removes CORS error
 app.use(cors());
@@ -30,7 +37,23 @@ app.get('/data', function(req, res) {
     }
     let results = data.searchText(columns, values);
     res.status(200).json({"results": results});
-    console.log('Data sent to Client.');
+});
+
+app.put('/data', function(req, res) {
+    let video_id = req.query["video_id"];
+    console.log(video_id);
+    if(video_id == undefined){
+      res.status(405).json({"error": "request video_id was blank"});
+    }
+    console.log(video_id);
+    let index = data.searchIndex([0], [video_id]);
+    console.log(index);
+    if(index.length != 1){
+      res.status(405).json({"error": "request video_id not found"});
+    }
+    else{
+      res.status(200).json({"results": "Nothing Happened Yet"});
+    }
 });
 
 app.listen(port, function() {
